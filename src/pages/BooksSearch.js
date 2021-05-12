6ee550cf0cf3a09844bd376793874291b5672b91
Omar Sakr <mero.saker@gmail.com>
2021-05-12 18:01:56 +0200

    feat: Manage input state in search

--- src/pages/BooksSearch.js
+++ src/pages/BooksSearch.js
@@ -2,23 +2,39 @@ import React , {Component} from 'react'
 import { Link } from 'react-router-dom'
 
 class BooksSearch extends Component{
-    render(){
-        return(
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to='/'>Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-                
-              </ol>
+
+  state = {
+    userInput : ''
+  }
+
+  handleChange = input => {
+    this.setState({
+      userInput: input
+    })
+  }
+
+  render(){
+      return(
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to='/'>Close</Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value = {this.state.userInput}
+                onChange = {(e) => this.handleChange(e.target.value)}
+              />
             </div>
           </div>
-        )
-    }
+          <div className="search-books-results">
+            <ol className="books-grid">
+              
+            </ol>
+          </div>
+        </div>
+      )
+  }
 }
 
 export default BooksSearch
