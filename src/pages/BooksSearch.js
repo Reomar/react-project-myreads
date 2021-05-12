66684ca0c63a440d9ebc322b094e530f1c7c4471
Omar Sakr <mero.saker@gmail.com>
2021-05-12 19:13:28 +0200

    feat: Dynamicly search books

--- src/pages/BooksSearch.js
+++ src/pages/BooksSearch.js
@@ -1,16 +1,34 @@
 import React , {Component} from 'react'
 import { Link } from 'react-router-dom'
+import { search } from '../BooksAPI'
+import Book from '../components/Book'
 
 class BooksSearch extends Component{
 
   state = {
-    userInput : ''
+    userInput : '',
+    books: []
   }
 
   handleChange = input => {
     this.setState({
       userInput: input
     })
+
+    this.searchBooks(input)
+  }
+
+  // Get Data from The API
+  searchBooks = (input) =>{
+    const userInput = this.state.userInput
+
+    if(input){
+      search(input).then(res => {
+        res.error ? this.setState({books: []}) : this.setState({books: res})
+      })
+    }
+
+    if (input == 0) this.setState({books: []})
   }
 
   render(){
@@ -29,7 +47,11 @@ class BooksSearch extends Component{
           </div>
           <div className="search-books-results">
             <ol className="books-grid">
-              
+                  { this.state.books.length > 0 &&
+                    this.state.books.map(book => (
+                      < Book book={book} />
+                    ))
+                  }
             </ol>
           </div>
         </div>
