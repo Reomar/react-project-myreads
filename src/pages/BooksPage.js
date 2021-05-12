35b2768ecb68e3359b97837d70255b8592b81f5e
Omar Sakr <mero.saker@gmail.com>
2021-05-12 19:51:54 +0200

    Remove mockup data

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -10,33 +10,7 @@ class BooksPage extends Component {
      * currentlyReading
      */
     state = {
-        books : [
-            {
-            id: 'nggnmAEACAAJ',
-            title: '1776',
-            authors: ['David McCullough', 'omar'],
-            imgURL: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-            shelf: 'wantToRead'
-        },{
-            id: '1283',
-            title: 'Book 2',
-            authors: ['Omar'],
-            imgURL: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-            shelf: 'currentlyReading'
-        },{
-            id: 'ppsd',
-            title: 'Book 3',
-            authors: ['David McCullough'],
-            imgURL: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-            shelf: 'wantToRead'
-        },{
-            id: 'nggnm12314AEACAAJ',
-            title: 'Read',
-            authors: ['David McCullough'],
-            imgURL: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-            shelf: 'read'
-        }
-    ]
+        books : []
     }
 
     // TODO==> get books Data from the backend
